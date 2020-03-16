76f8b14cb78d149a54a5d04da28c5cf55aecd5f8
Laila Campbell <lailacampbell96@gmail.com>
2020-03-16 16:27:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end 
   
   def execute_transaction
-    if @sender.balance -= @amount && @status == "pending" && @sender.valid? && @receiver.valid?
+    if @sender.balance > @amount && @status == "pending" && @sender.valid? && @receiver.valid?
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
